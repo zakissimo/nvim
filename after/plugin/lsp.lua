136d7193cb0c1a076ced8eb809766f398cafaa7f
zakissimo <zakaria.habri@gmail.com>
2023-04-02 18:42:40 +0200

    Tweaking snippet binds

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -92,14 +92,14 @@ local cmp_mappings = cmp.mapping.preset.insert({
         behavior = cmp.ConfirmBehavior.Replace,
         select = true,
     }),
-    ["<C-k>"] = cmp.mapping(function(fallback)
+    ["<C-j>"] = cmp.mapping(function(fallback)
         if luasnip.expand_or_jumpable() then
             luasnip.expand_or_jump()
         else
             fallback()
         end
     end, { "i", "s" }),
-    ["<C-j>"] = cmp.mapping(function(fallback)
+    ["<C-k>"] = cmp.mapping(function(fallback)
         if luasnip.jumpable(-1) then
             luasnip.jump(-1)
         else
