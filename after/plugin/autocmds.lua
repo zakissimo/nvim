ecf20f91f85bac08e0c68392cba5d7a150866b2c
zakissimo <zakaria.habri@gmail.com>
2023-05-01 23:12:10 +0200

    start insert in terms

--- after/plugin/autocmds.lua
+++ after/plugin/autocmds.lua
@@ -32,6 +32,7 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 vim.api.nvim_create_autocmd({ "TermOpen" }, {
     group = vim.api.nvim_create_augroup("TermOpen", { clear = true }),
     callback = function()
+        vim.cmd("startinsert")
         vim.opt_local.number = false
         vim.opt_local.relativenumber = false
         if vim.fn.win_gettype() ~= "popup" then
