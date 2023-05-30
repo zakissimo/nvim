1fe3c29fd1dd7c2046192618de6422ea097ef410
zakissimo <zakaria.habri@gmail.com>
2023-05-30 20:44:28 +0200

    see prev

--- lua/zak/lazy.lua
+++ lua/zak/lazy.lua
@@ -166,23 +166,23 @@ require("lazy").setup({
         branch = "v1.x",
         dependencies = {
             -- LSP Support
-            { "neovim/nvim-lspconfig" },   -- Required
+            { "neovim/nvim-lspconfig" }, -- Required
             { "williamboman/mason.nvim" }, -- Optional
             "jose-elias-alvarez/null-ls.nvim",
             "jay-babu/mason-null-ls.nvim",
             { "williamboman/mason-lspconfig.nvim" }, -- Optional
 
             -- Autocompletion
-            { "hrsh7th/nvim-cmp" },         -- Required
-            { "hrsh7th/cmp-nvim-lsp" },     -- Required
-            { "hrsh7th/cmp-buffer" },       -- Optional
-            { "hrsh7th/cmp-path" },         -- Optional
+            { "hrsh7th/nvim-cmp" }, -- Required
+            { "hrsh7th/cmp-nvim-lsp" }, -- Required
+            { "hrsh7th/cmp-buffer" }, -- Optional
+            { "hrsh7th/cmp-path" }, -- Optional
             { "saadparwaiz1/cmp_luasnip" }, -- Optional
-            { "hrsh7th/cmp-nvim-lua" },     -- Optional
-            { "onsails/lspkind-nvim" },     -- Optional
+            { "hrsh7th/cmp-nvim-lua" }, -- Optional
+            { "onsails/lspkind-nvim" }, -- Optional
 
             -- Snippets
-            { "L3MON4D3/LuaSnip" },             -- Required
+            { "L3MON4D3/LuaSnip" }, -- Required
             { "rafamadriz/friendly-snippets" }, -- Optional
         },
     },
