907373d9545bbd2fdabb976091529ef573199a1c
zakissimo <zakaria.habri@gmail.com>
2023-06-10 00:13:25 +0200

    We don't need all parsers

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -4,7 +4,8 @@ if not status_ok then
 end
 
 configs.setup({
-    ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+
+    ensure_installed = { "c", "cpp", "rust", "python", "typescript", "javascript", "lua", "vim", "vimdoc", "query" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
     ignore_install = { "" }, -- List of parsers to ignore installing
     highlight = {
