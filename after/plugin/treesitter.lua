4c7f689c35724e41a6909a494c5dc16f2d7f757e
zakissimo <zakaria.habri@gmail.com>
2023-06-13 20:13:31 +0200

    Input lang parsers manually in TS config file

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -3,27 +3,26 @@ if not status_ok then
     return
 end
 
-local mason_langs = {}
-local raw_lang_data = require("mason-registry").get_installed_packages()
-for _, lang in pairs(raw_lang_data) do
-    for key, value in pairs(lang) do
-        if key == "spec" then
-            mason_langs = vim.tbl_deep_extend("force", mason_langs, value.languages)
-        end
-    end
-end
-
-local hash = {}
-local langs = {}
-for _, v in ipairs(mason_langs) do
-    if not hash[v] then
-        langs[#langs + 1] = v:lower()
-        hash[v] = true
-    end
-end
-
 configs.setup({
-    ensure_installed = langs, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    ensure_installed = {
+        "c",
+        "cpp",
+        "lua",
+        "vim",
+        "python",
+        "javascript",
+        "typescript",
+        "query",
+        "css",
+        "scss",
+        "html",
+        "dockerfile",
+        "go",
+        "json",
+        "php",
+        "regex",
+        "yaml",
+    }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
     ignore_install = { "" }, -- List of parsers to ignore installing
     highlight = {
