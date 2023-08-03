cbb731a6f37e4c9d4e67b505a6300f6cff26bc5f
zakissimo <zakaria.habri@gmail.com>
2023-08-03 11:30:15 +0200

    Adding emoji plugin and del misc.lua (use utils.lua for now)

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -23,7 +23,53 @@ return {
     {
         "kylechui/nvim-surround",
         config = function()
-            require("nvim-surround").setup({})
+            require("nvim-surround").setup()
         end,
+    },
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        opts = {
+            enable_check_bracket_line = true,
+            ignored_next_char = "[%w%.]",
+            fast_wrap = {
+                map = "<M-e>",
+                chars = { "{", "[", "(", '"', "'" },
+                pattern = [=[[%'%"%>%]%)%}%,]]=],
+                end_key = "$",
+                keys = "qwertyuiopzxcvbnmasdfghjkl",
+                check_comma = true,
+                manual_position = true,
+                highlight = "Search",
+                highlight_grey = "Comment",
+            },
+        },
+    },
+    {
+        "ziontee113/icon-picker.nvim",
+        config = function()
+            require("icon-picker").setup({
+                disable_legacy_commands = true,
+            })
+            vim.keymap.set("n", "<Leader><Leader>i", "<cmd>IconPickerNormal<cr>")
+        end,
+    },
+
+    {
+        "zakissimo/run.nvim",
+        config = function()
+            vim.keymap.set("n", "<Leader>cc", "<CMD>lua require'run'.cmd()<CR>")
+        end,
+        dev = false,
+    },
+    {
+        "zakissimo/term.nvim",
+        config = function()
+            require("term").setup({
+                spawn = false,
+            })
+            vim.keymap.set({ "n", "t" }, "<Leader>t", "<CMD>lua require'term'.toggle()<CR>")
+        end,
+        dev = false,
     },
 }
