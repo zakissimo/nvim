d6cc7211960936c8349f742ba9f57429c0c41d1d
zakissimo <zakaria.habri@gmail.com>
2023-08-03 23:37:04 +0200

    added lsp lines

--- lua/plugins/nvim-lsp.lua
+++ lua/plugins/nvim-lsp.lua
@@ -22,6 +22,13 @@ return {
 
             { "jose-elias-alvarez/null-ls.nvim" },
             { "jay-babu/mason-null-ls.nvim" },
+            {
+                "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+                config = function()
+                    require("lsp_lines").setup()
+                    vim.keymap.set("", "<Leader>l", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
+                end,
+            },
         },
     },
     {
