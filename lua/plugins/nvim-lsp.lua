59d2034473ec83e0501c48ccd4cc0339ebf614f1
zakissimo <zakaria.habri@gmail.com>
2023-09-15 22:24:07 +0200

    nvim-ts-autotag

--- lua/plugins/nvim-lsp.lua
+++ lua/plugins/nvim-lsp.lua
@@ -37,6 +37,9 @@ return {
 
             {
                 "windwp/nvim-ts-autotag",
+                config = function()
+                    require("nvim-ts-autotag").setup()
+                end
             },
         },
     },
