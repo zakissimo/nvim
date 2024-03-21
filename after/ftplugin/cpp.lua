a766d5c2ecdb080dafc866629742d5494503513b
zakissimo <zakaria.habri@gmail.com>
2024-03-21 07:32:02 +0100

    Tweaks for cpp make command

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,12 +1,27 @@
-local root_dir = vim.fs.dirname(vim.fs.find({ "build.sh", "Makefile", ".git" }, { upward = true })[1])
+local build_files = { "build.sh", "Makefile", "CMakeLists.txt" }
+
+local root_dir = vim.fs.dirname(vim.fs.find(build_files, { upward = true })[1])
+
+local M = {}
+
+M.build = function(file)
+    if file == "build.sh" then
+        return "sh build.sh"
+    end
+    if file == "Makefile" then
+        return "make"
+    end
+    if file == "CMakeLists.txt" then
+        return "make"
+    end
+end
 
 if root_dir ~= nil then
-    local Makefile_path = root_dir .. "/Makefile"
-    local build_sh_path = root_dir .. "/build.sh"
-    if vim.fn.filereadable(Makefile_path) == 1 then
-        vim.opt.makeprg = "make"
-    elseif vim.fn.filereadable(build_sh_path) == 1 then
-        vim.opt.makeprg = "sh build.sh"
+    for _, build_file in pairs(build_files) do
+        local build_file_path = root_dir .. build_file
+        if vim.fn.filereadable(build_file_path) == 1 then
+            vim.opt.makeprg = M.build(build_file)
+        end
     end
 else
     vim.opt.makeprg = "c++ -Wall -Wextra -ggdb %:p:h/*.cpp -o main && ./main"
