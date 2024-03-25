86203048e087cc1a6088ad7c49818df453df95fc
zakissimo <zakaria.habri@gmail.com>
2024-03-25 17:08:32 +0100

    Don't forget to add dependencies !?!?!?!

--- lua/plugins/nvim-lsp.lua
+++ lua/plugins/nvim-lsp.lua
@@ -10,7 +10,7 @@ return {
             { "nvim-lua/plenary.nvim" },
 
             { "mfussenegger/nvim-dap" },
-            { "rcarriga/nvim-dap-ui" },
+            { "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } },
             {
                 "theHamsta/nvim-dap-virtual-text",
                 config = function()
