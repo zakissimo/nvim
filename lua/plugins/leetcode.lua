ea1c67e51949017ebcbb258338c8e4ecb72b7711
zakissimo <zakaria.habri@gmail.com>
2024-04-21 16:57:08 +0200

    Clean up

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -5,11 +5,6 @@ return {
         "nvim-telescope/telescope.nvim",
         "nvim-lua/plenary.nvim", -- required by telescope
         "MunifTanjim/nui.nvim",
-
-        -- optional
-        "nvim-treesitter/nvim-treesitter",
-        "rcarriga/nvim-notify",
-        "nvim-tree/nvim-web-devicons",
     },
     opts = {
         lang = "cpp",
--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -1,5 +1,12 @@
 return {
     {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = true,
+        -- use opts = {} for passing setup options
+        -- this is equalent to setup({}) function
+    },
+    {
         "sbulav/nredir.nvim",
     },
     {
