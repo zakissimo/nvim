e29168c8dda952aab210e762ac1805183167a52f
zakissimo <zakaria.habri@gmail.com>
2024-04-22 12:51:58 +0200

    Revert "Clean up"
    This reverts commit ea1c67e51949017ebcbb258338c8e4ecb72b7711.

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -5,6 +5,11 @@ return {
         "nvim-telescope/telescope.nvim",
         "nvim-lua/plenary.nvim", -- required by telescope
         "MunifTanjim/nui.nvim",
+
+        -- optional
+        "nvim-treesitter/nvim-treesitter",
+        "rcarriga/nvim-notify",
+        "nvim-tree/nvim-web-devicons",
     },
     opts = {
         lang = "cpp",
