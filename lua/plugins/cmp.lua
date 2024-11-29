9518ea2ebef4c891ab96bc91045e424a3fdc9467
zakissimo <zakaria.habri@gmail.com>
2024-11-29 23:17:25 +0100

    feat: facelifting part one

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,19 +1,33 @@
 return {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-cmdline",
-        "dmitmel/cmp-cmdline-history",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-nvim-lua",
-        "f3fora/cmp-spell",
-        "saadparwaiz1/cmp_luasnip",
-        "rafamadriz/friendly-snippets",
-        "hrsh7th/cmp-nvim-lsp-signature-help",
-        "hrsh7th/cmp-nvim-lsp-document-symbol",
-        "onsails/lspkind-nvim",
-        "L3MON4D3/LuaSnip",
-        "lukas-reineke/cmp-under-comparator",
+    {
+
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-nvim-lua",
+            "f3fora/cmp-spell",
+            "saadparwaiz1/cmp_luasnip",
+            "rafamadriz/friendly-snippets",
+            "hrsh7th/cmp-nvim-lsp-signature-help",
+            "hrsh7th/cmp-nvim-lsp-document-symbol",
+            "onsails/lspkind-nvim",
+            "L3MON4D3/LuaSnip",
+            "lukas-reineke/cmp-under-comparator",
+        },
     },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            cmdline = {
+                view = "cmdline"
+            }
+        },
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        }
+    }
 }
