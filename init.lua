da1aedc81873f09553e7d728fcb67cd09b077070
Zakaria Habri <zakaria.habri@gmail.com>
2025-06-01 23:07:01 +0200

    fix(diagnostics): no lines no text

--- init.lua
+++ init.lua
@@ -14,7 +14,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 vim.api.nvim_create_user_command("FormatDisable", function(args)
   if args.bang then
-    -- FormatDisable! will disable formatting just for this buffer
     vim.b.disable_autoformat = true
   else
     vim.g.disable_autoformat = true
@@ -23,6 +22,7 @@ end, {
   desc = "Disable autoformat-on-save",
   bang = true,
 })
+
 vim.api.nvim_create_user_command("FormatEnable", function()
   vim.b.disable_autoformat = false
   vim.g.disable_autoformat = false
@@ -32,7 +32,7 @@ end, {
 
 vim.diagnostic.config({
   virtual_text = false,
-  virtual_lines = true,
+  virtual_lines = false,
   signs = {
     text = {
       [vim.diagnostic.severity.ERROR] = " ",
