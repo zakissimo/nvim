e58e19b9d53dcc3fb32500d38858ba13a94f02a7
Zakaria Habri <zakaria.habri@gmail.com>
2025-06-15 12:20:44 +0200

    feat: neogit? + <leader>d for diffview

--- lua/config/plugins/version-control.lua
+++ lua/config/plugins/version-control.lua
@@ -1,4 +1,5 @@
 return {
+  { "NeogitOrg/neogit" },
   {
     "kdheepak/lazygit.nvim",
     lazy = true,
@@ -16,7 +17,7 @@ return {
   {
     "sindrets/diffview.nvim",
     keys = {
-      { "<leader>t", "<cmd>DiffviewFileHistory %<cr>" },
+      { "<leader>d", "<cmd>DiffviewFileHistory %<cr>" },
     },
   },
   {
