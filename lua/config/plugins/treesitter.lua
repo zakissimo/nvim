9a40ac09cd00ccccc14948f3f3eb36ffb1d72495
Zakaria Habri <zakaria.habri@gmail.com>
2026-02-17 17:33:42 +0100

    feat(treesitter): use autocmd to setup

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -2,18 +2,5 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function()
-      require('nvim-treesitter.configs').setup({
-        modules = {},
-        sync_install = false,
-        ignore_install = {},
-        auto_install = true,
-
-        ensure_installed = { "c", "cpp", "rust", "markdown", "lua", "vim", "vimdoc", "query" },
-        highlight = {
-          enable = true,
-        },
-      })
-    end,
   },
 }
