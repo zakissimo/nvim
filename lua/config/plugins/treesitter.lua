b77d7a5cfb24e621c56ec4293b84788eefaac63f
Zakaria Habri <zakaria.habri@gmail.com>
2026-02-17 16:59:18 +0100

    fix(treesitter): upstream change

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter.configs").setup({
+      require("nvim-treesitter").setup({
         ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
         sync_install = false,
         auto_install = false,
